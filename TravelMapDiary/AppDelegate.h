bbc581fc389a72a34410692f33b5cc7c9956c645
Eugene Belinski <eugeniub@gmail.com>
2015-02-14 13:04:05 -0600

    Initial Commit

---
+++ TravelMapDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravelMapDiary
+//
+//  Created by HHWS on 14/2/15.
+//  Copyright (c) 2015 Eugene Belinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
